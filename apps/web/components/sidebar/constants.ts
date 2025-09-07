d6fa411d6d85004860951c072ad4dfbc5b46db2f
bignosecss <bignosecss@gmail.com>
2025-09-07 10:02:51 +0800

    chore: diable search item temporarily

--- apps/web/components/sidebar/constants.ts
+++ apps/web/components/sidebar/constants.ts
@@ -1,11 +1,11 @@
 import { SquarePen, Search } from "lucide-react";
 
 export const HEAD_MENU_ITEMS = [
-  {
-    title: "搜索",
-    icon: Search,
-    operation: "search",
-  },
+  // {
+  //   title: "搜索",
+  //   icon: Search,
+  //   operation: "search",
+  // },
   {
     title: "新目标",
     icon: SquarePen,
