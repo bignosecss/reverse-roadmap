7387b170065ddc0ef61bc0c1812291a993af4d54
bignosecss <bignosecss@gmail.com>
2025-09-18 15:33:34 +0800

    fix: 无法正确在 minimap-tiptap/styles/index.css 中导入 tailwindcss 入口文件 app/globals.css
    - 临时修改UI，仅为了防止样式崩溃
    - 功能完成后，再考虑UI/UX

--- apps/web/components/canvas/tiptap.tsx
+++ apps/web/components/canvas/tiptap.tsx
@@ -3,13 +3,14 @@
 import { useState } from "react";
 import { Content } from "@tiptap/react";
 import { MinimalTiptapEditor } from "../ui/minimal-tiptap";
+import data from './content.json';
 
 export const Tiptap = () => {
   const [value, setValue] = useState<Content>("");
 
   return (
     <MinimalTiptapEditor
-      value={value}
+      value={data}
       onChange={setValue}
       className="w-full"
       editorContentClassName="p-5"
