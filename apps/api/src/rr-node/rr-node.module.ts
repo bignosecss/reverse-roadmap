17a4e95267ffe406b4a729512b0a279ee54fc676
bignosecss <bignosecss@gmail.com>
2025-09-20 11:18:20 +0800

    feat: 删除节点或树的同时删除其 rr-node-content(s)

--- apps/api/src/rr-node/rr-node.module.ts
+++ apps/api/src/rr-node/rr-node.module.ts
@@ -5,9 +5,11 @@ import { RrNodeController } from './rr-node.controller';
 import { RrNode, RrNodeSchema } from 'src/schemas/rr-node.schema';
 import { RrNodeMapper } from './mapper/rr-node.mapper';
 import { RrRoot, RrRootSchema } from 'src/schemas/rr-root.schema';
+import { RrNodeContentModule } from 'src/rr-node-content/rr-node-content.module';
 
 @Module({
   imports: [
+    RrNodeContentModule,
     MongooseModule.forFeature([
       { name: RrNode.name, schema: RrNodeSchema },
       { name: RrRoot.name, schema: RrRootSchema },
