c725445f2f64bf5ca35fdd7802a6d8ae1b10706a
bignosecss <bignosecss@gmail.com>
2025-10-24 12:39:17 +0800

    chore: 修改 dagre 计算布局默认使用的尺寸

--- apps/web/lib/flow-tree/dagre-layout.ts
+++ apps/web/lib/flow-tree/dagre-layout.ts
@@ -2,8 +2,8 @@ import dagre from "@dagrejs/dagre";
 import { Position } from "@xyflow/react";
 import { FlowData, FlowEdge, FlowNode } from "../types/models";
 
-const NODE_WIDTH = 240;
-const NODE_HEIGHT = 142;
+const NODE_WIDTH = 250;
+const NODE_HEIGHT = 150;
 const dagreGraph = new dagre.graphlib.Graph().setDefaultEdgeLabel(() => ({}));
 
 export enum DagreDirection {
