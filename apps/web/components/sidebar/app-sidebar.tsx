6c8a3cbf4de101b9d9981a8810e714bf5f68c608
bignosecss <bignosecss@gmail.com>
2025-11-12 19:50:23 +0800

    refactor: 拿掉 sidebar footer
    - 因为当前拿掉了登录模块开发的 todo

--- apps/web/components/sidebar/app-sidebar.tsx
+++ apps/web/components/sidebar/app-sidebar.tsx
@@ -3,14 +3,12 @@
 import { Sidebar } from "@/components/ui/sidebar";
 import { SidebarHeaderComponent } from "./sidebar-header";
 import { SidebarCustomContent } from "./sidebar-content";
-import { SidebarFooterComponent } from "./sidebar-footer";
 
 export default function AppSidebar() {
   return (
     <Sidebar collapsible="icon" className="flex flex-col">
       <SidebarHeaderComponent />
       <SidebarCustomContent />
-      <SidebarFooterComponent />
     </Sidebar>
   );
 }
