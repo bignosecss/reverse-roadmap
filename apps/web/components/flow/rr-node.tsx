3d6e9d952c5e6a9a56687bee49c79a28202e8b4c
bignosecss <bignosecss@gmail.com>
2025-11-20 15:27:49 +0800

    feat: private mode 节点才显示 toolbar

--- apps/web/components/flow/rr-node.tsx
+++ apps/web/components/flow/rr-node.tsx
@@ -2,9 +2,10 @@ import { useParams } from "next/navigation";
 import React from "react";
 import { NodeProps } from "@xyflow/react";
 
-import { FlowNode } from "@/lib/types/models";
+import { FlowNode, RrRootStatus } from "@/lib/types/models";
 import RrNodeToolbar from "./toolbar/rr-node-toolbar";
 import RrNodeCard from "./rr-node-card";
+import useSidebarStore from "@/lib/stores/sidebar";
 
 /**
  * 自定义 RrNode 组件
@@ -19,15 +20,18 @@ export default function RrNodeComponent({
   // 判断是否为根节点
   const treeId = useParams().id as string;
   const isRootNode = rrNode._id === treeId;
+  const mode = useSidebarStore((state) => state.mode);
 
   return (
     <>
       {/* 工具栏 */}
-      <RrNodeToolbar
-        isVisible={selected}
-        isRootNode={isRootNode}
-        currentNode={rrNode}
-      />
+      {mode === RrRootStatus.private && (
+        <RrNodeToolbar
+          isVisible={selected}
+          isRootNode={isRootNode}
+          currentNode={rrNode}
+        />
+      )}
 
       {/* 节点卡片 */}
       <RrNodeCard
