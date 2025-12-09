35dc90e6aa6ece7875db9189db11f699f4ac1971
bignosecss <bignosecss@gmail.com>
2025-12-09 11:25:10 +0800

    chore: 修改 api 模块的 eslint 配置
    - 并且重新 pnpm install

--- apps/api/eslint.config.mjs
+++ apps/api/eslint.config.mjs
@@ -1,16 +1,25 @@
-import { nestJsConfig } from '@repo/eslint-config/nest-js';
+import { nestJsConfig } from '@repo/eslint-config/nest';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  {
-    ignores: ['eslint.config.mjs'],
-  },
   ...nestJsConfig,
   {
-    languageOptions: {
-      parserOptions: {
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
+    ignores: [
+      'dist/**',
+      'build/**',
+      'coverage/**',
+      'node_modules/**',
+      '*.config.js',
+      '*.config.mjs',
+      '.next/**',
+      '.nuxt/**',
+      'public/dist/**',
+      'nuxt_dist/**',
+      '.output/**',
+      '.vercel/**',
+      '.netlify/**',
+      'vercel.json',
+      'netlify.json',
+    ],
   },
 );
