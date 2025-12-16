43bd499e3df69215eacbc873d6d1e218a918429a
bignosecss <bignosecss@gmail.com>
2025-12-16 19:51:27 +0800

    feat: add context aware chat

--- apps/rag/src/langchain-chat/langchain-chat.controller.ts
+++ apps/rag/src/langchain-chat/langchain-chat.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { LangchainChatService } from './langchain-chat.service';
 import { BasicMessageDto } from './dto/basic-message.dto';
+import { ContextAwareMessagesDto } from './dto/context-aware-messages.dto';
 
 @Controller('langchain-chat')
 export class LangchainChatController {
@@ -8,6 +9,15 @@ export class LangchainChatController {
 
   @Post('basic-chat')
   async chat(@Body() messagesDto: BasicMessageDto) {
-    return await this.langchainChatService.chat(messagesDto);
+    return await this.langchainChatService.basicChat(messagesDto);
+  }
+
+  @Post('context-aware-chat')
+  async contextAwareChat(
+    @Body() contextAwareMessagesDto: ContextAwareMessagesDto,
+  ) {
+    return await this.langchainChatService.contextAwareChat(
+      contextAwareMessagesDto,
+    );
   }
 }
--- apps/rag/src/langchain-chat/langchain-chat.service.ts
+++ apps/rag/src/langchain-chat/langchain-chat.service.ts
@@ -1,15 +1,17 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { HumanMessage, SystemMessage } from 'langchain';
+import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
+import { AIMessage, HumanMessage, SystemMessage } from 'langchain';
 import { ChatDeepSeek } from '@langchain/deepseek';
+import { BaseLanguageModelInput } from '@langchain/core/language_models/base';
 import { BasicMessageDto } from './dto/basic-message.dto';
 import { deepseekAI } from 'src/utils/constants/deepseek.constants';
 import { TEMPLATES } from 'src/utils/constants/templates.constants';
 import customMessage from 'src/utils/responses/customMessage.response';
 import { MESSAGES } from 'src/utils/constants/messages.constants';
+import { ContextAwareMessagesDto } from './dto/context-aware-messages.dto';
 
 @Injectable()
 export class LangchainChatService {
-  async chat(basicMessageDto: BasicMessageDto) {
+  async basicChat(basicMessageDto: BasicMessageDto) {
     try {
       const model = new ChatDeepSeek({
         temperature: +deepseekAI.BASIC_CHAT_DEEPSEEK_TEMPERATURE,
@@ -23,16 +25,43 @@ export class LangchainChatService {
 
       const response = await model.invoke(messages);
 
-      return customMessage(HttpStatus.OK, MESSAGES.SUCCESS, response.content);
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      return this.successResponse(response);
     } catch (e: unknown) {
-      throw new HttpException(
-        customMessage(
-          HttpStatus.INTERNAL_SERVER_ERROR,
-          MESSAGES.EXTERNAL_SERVER_ERROR,
-        ),
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
+      this.exceptionHandling(e);
     }
   }
+
+  async contextAwareChat(contextAwareMessagesDto: ContextAwareMessagesDto) {
+    try {
+      const model = new ChatDeepSeek({
+        temperature: +deepseekAI.BASIC_CHAT_DEEPSEEK_TEMPERATURE,
+        model: deepseekAI.DEEPSEEK_CHAT.toString(),
+      });
+
+      const systemMsg = new SystemMessage(
+        TEMPLATES.BASIC_SYSTEM_MESSAGE.toString(),
+      );
+      const messages = [systemMsg, ...contextAwareMessagesDto.messages];
+
+      const response = await model.invoke(messages as BaseLanguageModelInput);
+
+      return this.successResponse(response);
+    } catch (e: unknown) {
+      this.exceptionHandling(e);
+    }
+  }
+
+  private successResponse = (response: AIMessage) =>
+    customMessage(HttpStatus.OK, MESSAGES.SUCCESS, response);
+
+  private exceptionHandling = (e: unknown) => {
+    Logger.error(e);
+    throw new HttpException(
+      customMessage(
+        HttpStatus.INTERNAL_SERVER_ERROR,
+        MESSAGES.EXTERNAL_SERVER_ERROR,
+      ),
+      HttpStatus.INTERNAL_SERVER_ERROR,
+    );
+  };
 }
