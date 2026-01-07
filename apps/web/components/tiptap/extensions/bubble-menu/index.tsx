78b3c8b246c0477aebe6667d71beac96548b6cca
pcnf <138956102+bignosecss@users.noreply.github.com>
2026-01-07 23:08:38 +0800

    Feature/tiptap bubble menu (#25)
    * feat: add basic bubble-menu and typography extension
    
    * feat: beautiful bubble menu
    
    - add heading, list, and text formatting options to bubble menu
    - implement popver for text formatting controls(headings, lists)
    - add new button separator componentfor better UI organization
    - add text alignment extension support
    - update button styles with new iconsm size variant
    add placeholder AI chat feature in bubble menu
    
    * feat: enhance tiptap bubble menu with new popover components
    
    - replace inline popover with separate table-popoer and text-style-popover components
    - add tablekit extension with resizable table support
    - move text styling options (headings, lists) to text-style-popover
    - add table functionality through new table-popover component
    
    * feat: add underline support to tiptap bubble menu
    
    * chore: 修改 log

---
+++ apps/web/components/tiptap/extensions/bubble-menu/index.tsx
@@ -0,0 +1,124 @@
+import { Editor, useEditorState } from "@tiptap/react";
+import { BubbleMenu } from "@tiptap/react/menus";
+import { Button } from "@/components/ui/button";
+import {
+  FontBoldIcon,
+  FontItalicIcon,
+  QuoteIcon,
+  StrikethroughIcon,
+  UnderlineIcon,
+} from "@radix-ui/react-icons";
+import { ButtonSeparator } from "./button-separator";
+import TextStylePopover from "./text-style-popover";
+import TablePopover from "./table-popover";
+import { toast } from "sonner";
+
+export default function CustomBubbleMenu({ editor }: { editor: Editor }) {
+  const {
+    isBold,
+    isItalic,
+    isStrikethrough,
+    isUnderline,
+    isHeading1,
+    isHeading2,
+    isHeading3,
+    isHeading4,
+    isHeading5,
+    isHeading6,
+    isBulletList,
+    isOrderedList,
+  } = useEditorState({
+    editor,
+    selector: (ctx) => ({
+      isBold: ctx.editor.isActive("bold"),
+      isItalic: ctx.editor.isActive("italic"),
+      isStrikethrough: ctx.editor.isActive("strike"),
+      isUnderline: ctx.editor.isActive("underline"),
+      isHeading1: ctx.editor.isActive("heading", { level: 1 }),
+      isHeading2: ctx.editor.isActive("heading", { level: 2 }),
+      isHeading3: ctx.editor.isActive("heading", { level: 3 }),
+      isHeading4: ctx.editor.isActive("heading", { level: 4 }),
+      isHeading5: ctx.editor.isActive("heading", { level: 5 }),
+      isHeading6: ctx.editor.isActive("heading", { level: 6 }),
+      isBulletList: ctx.editor.isActive("bulletList"),
+      isOrderedList: ctx.editor.isActive("orderedList"),
+    }),
+  });
+
+  return (
+    <>
+      <BubbleMenu
+        editor={editor}
+        options={{ placement: "bottom", offset: 8, flip: true }}
+      >
+        <div className="bubble-menu">
+          <Button
+            variant="ghost"
+            size="iconsm"
+            onClick={() =>
+              toast.info("Talk to AI", {
+                description: "正在开发...",
+                position: "top-center",
+              })
+            }
+            className="w-fit px-1.5"
+            type="button"
+          >
+            <QuoteIcon />
+            聊聊～
+          </Button>
+          <ButtonSeparator />
+          <Button
+            variant="ghost"
+            size="iconsm"
+            onClick={() => editor.chain().focus().toggleBold().run()}
+            className={isBold ? "is-active" : ""}
+            type="button"
+          >
+            <FontBoldIcon />
+          </Button>
+          <Button
+            variant="ghost"
+            size="iconsm"
+            onClick={() => editor.chain().focus().toggleItalic().run()}
+            className={isItalic ? "is-active" : ""}
+            type="button"
+          >
+            <FontItalicIcon />
+          </Button>
+          <Button
+            variant="ghost"
+            size="iconsm"
+            onClick={() => editor.chain().focus().toggleUnderline().run()}
+            className={isUnderline ? "is-active" : ""}
+            type="button"
+          >
+            <UnderlineIcon />
+          </Button>
+          <Button
+            variant="ghost"
+            size="iconsm"
+            onClick={() => editor.chain().focus().toggleStrike().run()}
+            className={isStrikethrough ? "is-active" : ""}
+            type="button"
+          >
+            <StrikethroughIcon />
+          </Button>
+          <ButtonSeparator />
+          <TablePopover editor={editor} />
+          <TextStylePopover
+            editor={editor}
+            isHeading1={isHeading1}
+            isHeading2={isHeading2}
+            isHeading3={isHeading3}
+            isHeading4={isHeading4}
+            isHeading5={isHeading5}
+            isHeading6={isHeading6}
+            isBulletList={isBulletList}
+            isOrderedList={isOrderedList}
+          />
+        </div>
+      </BubbleMenu>
+    </>
+  );
+}
--- apps/web/components/tiptap/index.tsx
+++ apps/web/components/tiptap/index.tsx
@@ -1,8 +1,12 @@
 import { useCallback, useEffect, useRef } from "react";
 import { useGetRrContentById } from "@/hooks/use-rr-content";
+import { useAutoSave } from "./hooks";
 import { Content, Editor, EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
-import { useAutoSave } from "./hooks";
+import Typography from "@tiptap/extension-typography";
+import TextAlign from "@tiptap/extension-text-align";
+import { TableKit } from "@tiptap/extension-table";
+import CustomBubbleMenu from "./extensions/bubble-menu";
 
 import "./styles/tiptap.css";
 
@@ -23,7 +27,12 @@ export default function Tiptap({ tabId }: TiptapProps) {
 
   // tiptap editor 仅初始化一次
   const editor = useEditor({
-    extensions: [StarterKit],
+    extensions: [
+      StarterKit,
+      Typography,
+      TextAlign.configure({ types: ["heading", "paragraph", "codeblock"] }),
+      TableKit.configure({ table: { resizable: true } }),
+    ],
     immediatelyRender: false,
     content: "",
     editorProps: {
@@ -55,5 +64,10 @@ export default function Tiptap({ tabId }: TiptapProps) {
     return <div className="w-2/3">加载中...</div>;
   }
 
-  return <EditorContent className="w-2/3" editor={editor} />;
+  return (
+    <>
+      {!!editor && <CustomBubbleMenu editor={editor} />}
+      <EditorContent className="w-2/3" editor={editor} />
+    </>
+  );
 }
