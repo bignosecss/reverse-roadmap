c3f65673ae692b960e62e3c5f277ba59138eb82d
bignosecss <bignosecss@gmail.com>
2026-01-07 10:27:11 +0800

    feat: 打开 Canvas 后设置 tab 对应的 content

--- apps/web/components/canvas/index.tsx
+++ apps/web/components/canvas/index.tsx
@@ -15,9 +15,10 @@ export function Canvas() {
 
   // 渲染 Tab 内容（业务层自定义）
   const renderTabContent = useCallback(
-    (tabId: number | string) => {
+    (tabId: string) => {
       const currentTab = tabs.find((tab) => tab.id === tabId);
-      if (!currentTab) return <div className="text-gray-500">暂无内容</div>;
+      if (!currentTab)
+        return <div className="text-muted-foreground">暂无内容</div>;
 
       return (
         <div className="flex flex-col items-center justify-center size-full">
@@ -25,12 +26,35 @@ export function Canvas() {
           <p className="text-muted-foreground mb-4">
             {currentRrNode?.description}
           </p>
-          <Tiptap />
+          <Tiptap tabId={tabId} />
         </div>
       );
     },
     [currentRrNode, tabs],
   );
+
+  // transition 属性，只是将组件移动走了；但组件的宽度坑位还在
+  // 并没有解决占位的问题
+  // 为了保持打开动画，并减少关闭时不必要的请求、计算，暂时使用字符占位
+  if (!canvasOpen) {
+    return (
+      <div
+        className={cn(
+          "overflow-hidden",
+          "md:shadow-[0_0_18px_var(--border)]",
+          "transition-all duration-300 ease-in-out",
+          {
+            "basis-2/3 translate-x-0 opacity-100": canvasOpen,
+            "basis-0 translate-x-full opacity-0 pointer-events-none":
+              !canvasOpen,
+          },
+        )}
+      >
+        temp solution
+      </div>
+    );
+  }
+  // TODO: 优化 canvas 关闭时候的处理
 
   return (
     <div
--- apps/web/components/canvas/tiptap/index.tsx
+++ apps/web/components/canvas/tiptap/index.tsx
@@ -1,12 +1,45 @@
-import { EditorContent, useEditor } from "@tiptap/react";
+import { useEffect, useRef } from "react";
+import { useGetRrContentById } from "@/hooks/use-rr-content";
+import { Content, EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 
-export default function Tiptap() {
+interface TiptapProps {
+  tabId: string;
+}
+
+export default function Tiptap({ tabId }: TiptapProps) {
+  const { data: rrContent, isLoading: isRrContentLoading } =
+    useGetRrContentById(tabId);
+  const editorRef = useRef<ReturnType<typeof useEditor> | null>(null);
+
+  // tiptap editor 仅初始化一次
   const editor = useEditor({
     extensions: [StarterKit],
     immediatelyRender: false,
-    content: "hello world",
+    content: "",
   });
+
+  useEffect(() => {
+    editorRef.current = editor;
+    return () => editor?.destroy();
+  }, [editor]);
+
+  useEffect(() => {
+    if (isRrContentLoading || !rrContent || !editorRef.current) return;
+
+    editorRef.current
+      .chain()
+      .clearContent()
+      .setContent(rrContent as Content)
+      .focus()
+      .run();
+
+    console.log("编辑器内容已更新：", rrContent);
+  }, [isRrContentLoading, rrContent]);
+
+  if (isRrContentLoading) {
+    return <div className="w-2/3">加载中...</div>;
+  }
 
   return <EditorContent className="w-2/3" editor={editor} />;
 }
