5621bd3948ea51719e3d5fe9af25e4984fb812fe
bignosecss <bignosecss@gmail.com>
2026-02-03 17:47:13 +0800

    chore(rag): remove old loader

--- apps/rag/src/loaders/loader.service.ts
+++ apps/rag/src/loaders/loader.service.ts
@@ -3,24 +3,4 @@ import { RecursiveCharacterTextSplitter } from '@langchain/textsplitters';
 
 @Injectable()
 export class LoaderService {
-  async rawTextLoader(nativeRawTextDocument: string): Promise<string[]> {
-    const splitter = new RecursiveCharacterTextSplitter({
-      chunkSize: 1800,
-      chunkOverlap: 200,
-      separators: ['\n\n'],
-    });
-    const textChunks = await splitter.splitText(nativeRawTextDocument);
-
-    return textChunks.map((chunk) => this.cleanString(chunk));
-  }
-
-  private cleanString(text: string) {
-    text = text.replace(/\\/g, '');
-    text = text.replace(/#/g, ' ');
-    text = text.replace(/\. \./g, '.');
-    text = text.replace(/\s\s+/g, ' ');
-    text = text.replace(/(\r\n|\n|\r)/gm, ' ');
-
-    return text.trim();
-  }
 }
